7567ed3e06142040651c405fc5a3264c28cb21f6
swamyshogan <shoganswamy@gmail.com>
2024-11-10 04:12:47 -0500

    DNA, RNA and REVC

---
+++ src/REVC.py
@@ -0,0 +1,21 @@
+"""
+Rosalind Code: REVC
+By Shogan
+"""
+
+def revc(dna:str):
+    # Create the complementary strand
+    rcdna = dna[::-1].upper().translate(str.maketrans('ATCG', 'TAGC'))
+    
+    return rcdna
+
+def main():
+    # input
+    with open("rosalind_revc.txt", 'r') as file:
+        s = file.read().strip()
+
+    # output
+    print(revc(s))
+
+if __name__=='__main__':
+    main()
---
+++ src/RNA.py
@@ -0,0 +1,26 @@
+"""
+Rosalind Code: RNA
+By: Shogan
+"""
+
+def main():
+    # input
+    with open("rosalind_rna.txt", 'r') as file:
+        t = file.read().strip()
+
+    # main logic
+    if t.isalpha():
+        u = t.upper().replace('T', 'U')
+        # output
+        print("RNA is \n", u)
+    else:
+        print("INVALID INPUT FORMAT")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
